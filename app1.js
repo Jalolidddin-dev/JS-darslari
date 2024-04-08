7d5585230c717a004d0f3be9f4a8be33146cde07
Jaloliddin <jaloliddinabdurahmmonov13@gmail.com>
2024-04-08 16:44:46 +0500

    added first lesson

---
+++ app1.js
@@ -0,0 +1,79 @@
+// o`zgaruvchi
+// var let const -- yangi o'zgaruvchi yaratilsa
+
+
+// ================ var ===============
+// var ism = "Jaloliddin1";
+
+
+// console.log(ism);
+
+
+// var yosh = 24
+
+// console.log(yosh);
+
+
+// var hobby = "Futboll"
+// console.log(hobby)
+
+
+// var isStudy = "ha"
+
+// console.log(isStudy);
+
+
+// =================== let ================
+
+// let name = "Hello world";
+// console.log(name)
+
+
+// name = "Jaloliddin"
+
+// console.log(name)
+
+
+
+// ============== const ================
+
+// const name = "Jaloliddin"
+
+// console.log(name)
+
+
+
+//   ============== let vs var ===============
+
+
+// var ism = 'Jaloliddin'
+
+// var ism = "Mirsaid"
+
+// var ism = "Ibrohim"
+
+// var ism = "Husanboy"
+
+// var ism = "Islomjon"
+
+// console.log(ism)
+
+
+
+// let name = "Jaloliddin"
+// // let name = "Abbos"
+// console.log(name)
+
+// console.log(ism)
+
+// var ism = "Jaloliddin"
+
+
+// console.log(name)
+
+// let name = "Abbos"
+
+
+
+
+
